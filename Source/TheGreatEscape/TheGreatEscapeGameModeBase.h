1f0fb018cc4ffc4fe1a779a467a8d71700cbc434
Davinderpal Cheema <dcheema6@uwo.ca>
2019-01-29 19:46:54 -0500

    inital commit

---
+++ Source/TheGreatEscape/TheGreatEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheGreatEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEGREATESCAPE_API ATheGreatEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
